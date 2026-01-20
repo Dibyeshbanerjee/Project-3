6c77242e4efa8675925d7244780a4f5e7de7fb9f
Dibyeshbanerjee <105985392+Dibyeshbanerjee@users.noreply.github.com>
2026-01-20 19:52:35 +0530

    Create task2_math_module.py

---
+++ task2_math_module.py
@@ -0,0 +1,13 @@
+# Task 2: Using the Math Module for Calculations
+
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+print("Square root:", square_root)
+print("Natural logarithm:", log_value)
+print("Sine:", sine_value)
